3a5cf2de2e9556466938e6a33ee385b184fd8e36
Mat Fish <matanya@daneltech.com>
2022-02-26 14:05:32 +0000

    minify assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@
 
 let mix = require('laravel-mix');
 mix.setPublicPath('src/assets/compiled');
-mix.js('src/assets/site/js/main.js', 'craft-tablecloth.min.js');
-mix.js('src/assets/cp/js/cp.js', 'craft-tablecloth-cp.min.js');
+mix.js('src/assets/site/js/main.js', 'craft-tablecloth.js');
+mix.js('src/assets/cp/js/cp.js', 'craft-tablecloth-cp.js');
+mix.minify('src/assets/compiled/craft-tablecloth.js')
+mix.minify('src/assets/compiled/craft-tablecloth-cp.js')
 
